130ade41340ee4b71bc92bd0281c1b77003cd67f
wty4427300 <wty4427300@gmail.com>
2023-08-11 15:41:40 +0800

    iterators

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,7 +6,7 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -24,7 +24,7 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    words.iter().map(|word| capitalize_first(word)).collect()
 }
 
 // Step 3.
@@ -32,7 +32,8 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    //因为有" "空字符串,所以join不做额外处理
+    words.iter().map(|word| capitalize_first(word)).collect::<Vec<String>>().join("")
 }
 
 #[cfg(test)]
